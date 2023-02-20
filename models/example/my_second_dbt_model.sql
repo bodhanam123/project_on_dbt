a68bf2033558db4256570f6ac68fccff14e0644c
bodhanam.pranavireddy@gmail.com <bodhanam.pranavireddy@gmail.com>
2023-02-20 03:46:06 +0000

    commit

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
---test2
-=======
-<<<<<<< HEAD
---test12
-=======
---test
->>>>>>> 1ee7505b7d6cf07f2d1180aef15ea0dd2949ccac
->>>>>>> 848d630b4dc318580a6be8e1782a63a4eac855e1
+
 -- Use the `ref` function to select from other models
 --
 select *
