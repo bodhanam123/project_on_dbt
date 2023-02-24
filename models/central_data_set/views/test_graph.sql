40e176e30e5b56d67f65133b35791945cb5afa50
bodhanam.pranavireddy@gmail.com <bodhanam.pranavireddy@gmail.com>
2023-02-24 09:06:47 +0000

    commit

--- models/central_data_set/views/test_graph.sql
+++
@@ -1,9 +0,0 @@
-{% if execute %}
-  {% for node in graph.nodes.values()
-     | selectattr("resource_type", "equalto", "model")
-     | selectattr("package_name", "equalto", "snowplow") %}
-  
-    {% do log(node.unique_id ~ ", materialized: " ~ node.config.materialized, info=true) %}
-  
-  {% endfor %}
-{% endif %}
